0adaa4b5fc76044d7e079a2416f93f82af69d57d
Sean Van Der Toorn <Sean.VanDerToorn@mixtelematics.com>
2017-06-05 16:17:33 +0200

    Initial Commit

---
+++ WebApplication/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApplication.Startup))]
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+
+            httpConfiguration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
---
+++ Webapplication.Test/ApiTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Owin.Testing;
+using System.Net.Http;
+
+namespace WebApplication.Test
+{
+    [TestClass]
+    public class ApiTests
+    {
+        protected TestServer _server;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create an OWIN test server
+            _server = TestServer.Create<Startup>();
+        }
+
+        [TestMethod]
+        public void TestApi()
+        {
+            // Get the HttpClient from the OWIN test server
+            HttpClient client = _server.HttpClient;
+
+            // Use a mock url, but use a valid route
+            var url = "http://mockserver/api/values";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var response = client.SendAsync(request).ConfigureAwait(false).GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            Assert.IsTrue(responseContent.Contains("value1"));
+            Assert.IsTrue(responseContent.Contains("value2"));
+        }
+    }
+}
